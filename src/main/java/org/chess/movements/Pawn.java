7fe1c4112a66b3fd7636ec4f026e914078f1c2aa
sidharth-k-7 <skrishna@email.com>
2025-08-23 23:16:13 +0530

    Validation for last positions of pawn

--- src/main/java/org/chess/movements/Pawn.java
+++ src/main/java/org/chess/movements/Pawn.java
@@ -3,6 +3,7 @@ package org.chess.movements;
 import org.chess.enums.Direction;
 import org.chess.models.Position;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Pawn extends ChessPiece{
@@ -13,6 +14,13 @@ public class Pawn extends ChessPiece{
 
     @Override
     public List<String> getMoves() {
+        //validating the last position movement for pawn (Ex. H8 no forward movement)
+        Position position = getPosition();
+        if(position.getRow()==8){
+            System.out.println("No valid moves available at the " +
+                    "given position. Try some other.!");
+            return Collections.emptyList();
+        }
         return generateMoves(Direction.forwardOnly(), true);
     }
 }
